18f3aee3f6c06f43b7943c3fdc74ed97a05f1a25
NNOGS <nnogswill@gmail.com>
2026-03-14 03:17:02 +0100

    week02 Resume/program.cs complete

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,63 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Journal Project.");
+
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Product Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2024;
+
+        Resume myResume = new Resume();
+        myResume._name = "John Smith";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
     }
+}
+    class Job
+    {
+       public string _company;
+       public string _jobTitle;
+       public int _startYear;
+       public int _endYear;
+    
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
+
+class Resume
+{
+    public string _name;
+    public List<Job> _jobs = new List<Job>();
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }
+    
+    }   
+
 }
